aaf122cdd78db4506cf06d00dd129f8cb7660331
saya6 <s.ayat@uniandes.edu.co>
2022-05-01 10:43:19 -0500

    [FEATURE] listar plantas - agregardo conteo de plantas por tipo, ajustado el html

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -9,16 +9,31 @@ import { PlantService } from '../plant.service';
 export class PlantListComponent implements OnInit {
 
   plants: Array<Plant> = [];
+  ocurrencies: Map<string, number> = new Map<string, number>();
   constructor(private plantService: PlantService) { }
 
   getPlants(): void {
     this.plantService.getPlants().subscribe((plants) => {
       this.plants = plants;
+      this.countPlants()
     });
   }
 
   ngOnInit() {
     this.getPlants();
   }
+
+  countPlants(){
+    this.plants.forEach((elem) => {
+      let currentValue = this.ocurrencies.get(elem.tipo)
+      if (currentValue === undefined) {
+        this.ocurrencies.set(elem.tipo, 1)
+      } else {
+        this.ocurrencies.set(elem.tipo, currentValue+1)
+      }
+    })
+  }
+
+
 
 }
